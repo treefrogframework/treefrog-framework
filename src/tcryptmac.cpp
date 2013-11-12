d2ee7b64b4a4850efa99c7715e1a74203f99638b
aoyama <a.kazuharu@gmail.com>
2013-11-13 01:01:20 +0900

    added HMAC_SHA256, HMAC_SHA384 and HMAC_SHA512 algorithm.

--- src/tcryptmac.cpp
+++ src/tcryptmac.cpp
@@ -6,12 +6,26 @@
  */
 
 #include <QCryptographicHash>
+#include <QHash>
 #include <TCryptMac>
+
+typedef QHash<int, int> BlockSizeHash;
+Q_GLOBAL_STATIC_WITH_INITIALIZER(BlockSizeHash, blockSizeHash,
+{
+    x->insert(TCryptMac::Hmac_Md5, 64);
+    x->insert(TCryptMac::Hmac_Sha1, 64);
+#if QT_VERSION >= 0x050000
+    x->insert(TCryptMac::Hmac_Sha256, 64);
+    x->insert(TCryptMac::Hmac_Sha384, 128);
+    x->insert(TCryptMac::Hmac_Sha512, 128);
+#endif
+})
+
 
 /*!
   \class TCryptMac
   \brief The TCryptMac class provides the functionality of a
-  "Message Authentication Code" (MAC) algorithm. 
+  "Message Authentication Code" (MAC) algorithm.
 */
 
 /*!
@@ -20,13 +34,14 @@
 */
 QByteArray TCryptMac::mac(const QByteArray &data, const QByteArray &key, Algorithm method)
 {
-    QByteArray tk = (key.length() > 64) ? QCryptographicHash::hash(key, (QCryptographicHash::Algorithm)method) : key;
-    QByteArray k_ipad(64, '\0');
+    int blockSize = blockSizeHash()->value(method);
+    QByteArray tk = (key.length() > blockSize) ? QCryptographicHash::hash(key, (QCryptographicHash::Algorithm)method) : key;
+    QByteArray k_ipad(blockSize, '\0');
     k_ipad.replace(0, tk.length(), tk);
-    QByteArray k_opad = k_ipad;    
+    QByteArray k_opad = k_ipad;
 
     // XOR key with ipad and opad values
-    for (int i = 0; i < 64; ++i) {
+    for (int i = 0; i < blockSize; ++i) {
         k_ipad[i] = k_ipad[i] ^ 0x36;
         k_opad[i] = k_opad[i] ^ 0x5c;
     }
--- src/tcryptmac.h
+++ src/tcryptmac.h
@@ -12,8 +12,13 @@ public:
     enum Algorithm {
         Hmac_Md5  = QCryptographicHash::Md5,
         Hmac_Sha1 = QCryptographicHash::Sha1,
+#if QT_VERSION >= 0x050000
+        Hmac_Sha256 = QCryptographicHash::Sha256,
+        Hmac_Sha384 = QCryptographicHash::Sha384,
+        Hmac_Sha512 = QCryptographicHash::Sha512,
+#endif
     };
-    
+
     static QByteArray mac(const QByteArray &data, const QByteArray &key, Algorithm method);
 };
 
