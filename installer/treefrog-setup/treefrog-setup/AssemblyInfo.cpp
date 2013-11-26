3e15d942b5cd94421f05cebea6618f2731b23b74
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2013-11-26 10:27:25 +0900

    updated the installer program.

--- installer/treefrog-setup/treefrog-setup/AssemblyInfo.cpp
+++ installer/treefrog-setup/treefrog-setup/AssemblyInfo.cpp
@@ -31,7 +31,7 @@ using namespace System::Security::Permissions;
 // すべての値を指定するか、下のように '*' を使ってリビジョンおよびビルド番号を
 // 既定値にすることができます:
 
-[assembly:AssemblyVersionAttribute("1.7.3")];
+[assembly:AssemblyVersionAttribute("1.7.4")];
 
 [assembly:ComVisible(false)];
 
