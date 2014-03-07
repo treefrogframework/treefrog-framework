0a3fce9cb82e251d813830880a49833534682071
Akos Vandra <avandra@whitepages.com>
2014-03-07 13:11:31 +0100

    fixed routing for get and post routes. Was not actually filtering and rejecting other http verbs

--- src/turlroute.cpp
+++ src/turlroute.cpp
@@ -114,35 +114,42 @@ bool TUrlRoute::parseConfigFile()
 
 TRouting TUrlRoute::findRouting(Tf::HttpMethod method, const QString &path) const
 {
+    QStringList params;
+
     for (QListIterator<TRoute> i(routes); i.hasNext(); ) {
         const TRoute &rt = i.next();
-        if (!rt.params && rt.path == path) {
-            if (rt.method != TRoute::Match
-                && (rt.method == TRoute::Get && method != Tf::Get)
-                && (rt.method == TRoute::Post && method != Tf::Post)) {
-                return TRouting("", "");  // reject routing
-            }
-            return TRouting(rt.controller, rt.action);
+
+        //If our target is not starting with the same string, there cannot be a match
+        if (!path.startsWith(rt.path)) continue;
+
+        //If we don't have the params set, we need to have an exact match.
+        if (!rt.params && (path != rt.path)) continue;
+
+        //Parse parameters
+        if (rt.params)
+        {
+            int len = rt.path.length();
+            QString paramstr = path.mid(len);
+            params = paramstr.split('/', QString::SkipEmptyParts);
         }
 
-        if (rt.params && path.startsWith(rt.path)) {
-            if (rt.method != TRoute::Match
-                && (rt.method == TRoute::Get && method != Tf::Get)
-                && (rt.method == TRoute::Post && method != Tf::Post)) {
-                return TRouting("", "");  // reject routing
-            }
+        //Check if we have a good http verb
+        switch(rt.method)
+        {
+            case TRoute::Match:
+                return TRouting(rt.controller, rt.action, params);
 
-            QStringList params;
-            int len = rt.path.endsWith('/') ? rt.path.length() : rt.path.length() + 1;
-            QString paramstr = path.mid(len);
-            if (!paramstr.isEmpty()) {
-                params = paramstr.split('/');
-                if (path.endsWith(QLatin1Char('/')) && !params.isEmpty()) {
-                    params.removeLast();  // unuse last item
-                }
-            }
-            return TRouting(rt.controller, rt.action, params);
+            case TRoute::Get:
+                if (method == Tf::Get) return TRouting(rt.controller, rt.action, params);
+                continue;
+            case TRoute::Post:
+                if (method == Tf::Post) return TRouting(rt.controller, rt.action, params);
+                continue;
+            default:
+                tSystemWarn("Unkown route method in findRouting: %d", rt.method);
+                continue;
         }
     }
+
     return TRouting();  // Not found routing info
 }
