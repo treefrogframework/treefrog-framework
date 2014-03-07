abe29f28549deff836a204ea2c878a636ada6151
Akos Vandra <avandra@whitepages.com>
2014-03-07 13:22:05 +0100

    indentation fix

--- src/troute.cpp
+++ src/troute.cpp
@@ -8,19 +8,19 @@ typedef QHash<int, QString> Method2StringHash;
 
 Q_GLOBAL_STATIC_WITH_INITIALIZER(String2MethodHash, String2Method,
 {
-    x->insert("MATCH", TRoute::Match);
-    x->insert("GET",   TRoute::Get);
-    x->insert("POST",  TRoute::Post);
-    x->insert("PUT",   TRoute::Put);
-    x->insert("PATCH",   TRoute::Patch);
+    x->insert("MATCH",    TRoute::Match);
+    x->insert("GET",      TRoute::Get);
+    x->insert("POST",     TRoute::Post);
+    x->insert("PUT",      TRoute::Put);
+    x->insert("PATCH",    TRoute::Patch);
     x->insert("DELETE",   TRoute::Delete);
 });
 
 Q_GLOBAL_STATIC_WITH_INITIALIZER(Method2StringHash, Method2String,
 {
-    x->insert(TRoute::Match, "MATCH");
-    x->insert(TRoute::Get,   "GET");
-    x->insert(TRoute::Post,  "POST");
+    x->insert(TRoute::Match,  "MATCH");
+    x->insert(TRoute::Get,    "GET");
+    x->insert(TRoute::Post,   "POST");
     x->insert(TRoute::Put,    "PUT");
     x->insert(TRoute::Patch,  "PATCH");
     x->insert(TRoute::Delete, "DELETE");
