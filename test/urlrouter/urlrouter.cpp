49d972add71eda3ec861ec26d119f737dceb1903
Akos Vandra <avandra@whitepages.com>
2014-03-30 12:51:39 -0700

    add automated requirement testing for full rest support

--- test/urlrouter/urlrouter.cpp
+++ test/urlrouter/urlrouter.cpp
@@ -22,6 +22,16 @@ private slots:
     void should_route_urls_with_parameters();
     void should_route_urls_with_empty_parameters();
     void should_route_urls_with_no_parameters();
+    void should_route_urls_with_single_parameters_correctly();
+    void should_not_route_urls_with_multiple_parameters_instead_of_single();
+    void should_route_urls_with_empty_single_parameters_correctly();
+    void should_route_urls_with_multiple_single_parameters_correctly();
+    void should_not_route_urls_with_missing_single_parameters();
+    void should_not_route_urls_if_static_part_is_not_matching();
+    void should_route_urls_with_both_single_and_multiple_parameters_correctly_when_multiple_params_is_empty();
+    void should_route_urls_with_both_single_and_multiple_parameters_correctly_when_multiple_params_is_not_empty();
+    void should_route_urls_with_both_single_and_multiple_parameters_correctly_when_multiple_params_has_multiple_items();
+    void should_not_accept_routes_with_params_in_middle();
 };
 
 void TestUrlRouter::init()
@@ -110,7 +120,7 @@ void TestUrlRouter::should_route_to_empty_if_no_method_matches()
 void TestUrlRouter::should_route_urls_with_parameters()
 {
     ur->addRouteFromString("GET  /:params 'dummy#index'");
-    TRouting r = ur->findRouting(Tf::Get, "/p1/p2/p3");
+    TRouting r = ur->findRouting(Tf::Get, "/p1/p2/p3/");
 
     QCOMPARE(QString(r.controller), QString("dummycontroller"));
     QCOMPARE(QString(r.action), QString("index"));
@@ -120,7 +130,7 @@ void TestUrlRouter::should_route_urls_with_parameters()
 void TestUrlRouter::should_route_urls_with_empty_parameters()
 {
     ur->addRouteFromString("GET  /:params 'dummy#index'");
-    TRouting r = ur->findRouting(Tf::Get, "/p1//p3");
+    TRouting r = ur->findRouting(Tf::Get, "/p1//p3/");
 
     QCOMPARE(QString(r.controller), QString("dummycontroller"));
     QCOMPARE(QString(r.action), QString("index"));
@@ -136,6 +146,96 @@ void TestUrlRouter::should_route_urls_with_no_parameters()
     QCOMPARE(QString(r.action), QString("index"));
     QCOMPARE(r.params, QStringList());
 }
+
+void TestUrlRouter::should_route_urls_with_single_parameters_correctly()
+{
+    ur->addRouteFromString("GET  /foo/:param/bar 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/bar/");
+
+    QCOMPARE(QString(r.controller), QString("dummycontroller"));
+    QCOMPARE(QString(r.action), QString("index"));
+    QCOMPARE(r.params, QStringList() << "p1");
+}
+
+void TestUrlRouter::should_not_route_urls_with_multiple_parameters_instead_of_single()
+{
+    ur->addRouteFromString("GET  /foo/:param/bar 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/p2/bar/");
+
+    QCOMPARE(r.isEmpty(), true);
+}
+
+void TestUrlRouter::should_route_urls_with_empty_single_parameters_correctly()
+{
+    ur->addRouteFromString("GET  /foo/:param/bar 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo//bar/");
+
+    QCOMPARE(QString(r.controller), QString("dummycontroller"));
+    QCOMPARE(QString(r.action), QString("index"));
+    QCOMPARE(r.params, QStringList() << "");
+}
+
+void TestUrlRouter::should_route_urls_with_multiple_single_parameters_correctly()
+{
+    ur->addRouteFromString("GET  /foo/:param/bar/:param/baz/:param 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/bar/p2/baz/p3/");
+
+    QCOMPARE(QString(r.controller), QString("dummycontroller"));
+    QCOMPARE(QString(r.action), QString("index"));
+    QCOMPARE(r.params, QStringList() << "p1" << "p2" << "p3");
+}
+
+void TestUrlRouter::should_not_route_urls_with_missing_single_parameters()
+{
+    ur->addRouteFromString("GET  /foo/:param/bar/ 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/bar/");
+
+    QCOMPARE(r.isEmpty(), true);
+}
+
+void TestUrlRouter::should_not_route_urls_if_static_part_is_not_matching()
+{
+    ur->addRouteFromString("GET  /foo/:param/bar/ 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/baz/");
+
+    QCOMPARE(r.isEmpty(), true);
+}
+
+void TestUrlRouter::should_route_urls_with_both_single_and_multiple_parameters_correctly_when_multiple_params_is_empty()
+{
+    ur->addRouteFromString("GET  /foo/:param/baz/:params 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/baz/");
+
+    QCOMPARE(QString(r.controller), QString("dummycontroller"));
+    QCOMPARE(QString(r.action), QString("index"));
+    QCOMPARE(r.params, QStringList() << "p1");
+}
+
+void TestUrlRouter::should_route_urls_with_both_single_and_multiple_parameters_correctly_when_multiple_params_is_not_empty()
+{
+    ur->addRouteFromString("GET  /foo/:param/baz/:params 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/baz/p2/");
+
+    QCOMPARE(QString(r.controller), QString("dummycontroller"));
+    QCOMPARE(QString(r.action), QString("index"));
+    QCOMPARE(r.params, QStringList() << "p1" << "p2");
+}
+
+void TestUrlRouter::should_route_urls_with_both_single_and_multiple_parameters_correctly_when_multiple_params_has_multiple_items()
+{
+    ur->addRouteFromString("GET  /foo/:param/baz/:params/ 'dummy#index'");
+    TRouting r = ur->findRouting(Tf::Get, "/foo/p1/baz/p2/p3/");
+
+    QCOMPARE(QString(r.controller), QString("dummycontroller"));
+    QCOMPARE(QString(r.action), QString("index"));
+    QCOMPARE(r.params, QStringList() << "p1" << "p2" << "p3");
+}
+
+void TestUrlRouter::should_not_accept_routes_with_params_in_middle()
+{
+    QCOMPARE(ur->addRouteFromString("GET /foo/:params/bar 'dummy#index'"), false);
+}
+
 
 QTEST_MAIN(TestUrlRouter)
 #include "urlrouter.moc"
