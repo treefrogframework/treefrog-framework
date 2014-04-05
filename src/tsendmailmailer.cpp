45a0e278fdc93c0a382021abd55d2b4f37b140b1
AOYAMA Kazuharu <aoyama@ideeinc.co.jp>
2014-04-05 21:11:15 +0900

    import SendmailMailer class.

---
+++ src/tsendmailmailer.cpp
@@ -0,0 +1,96 @@
+/* Copyright (c) 2014, AOYAMA Kazuharu
+ * All rights reserved.
+ *
+ * This software may be used and distributed according to the terms of
+ * the New BSD License, which is incorporated herein by reference.
+ */
+
+#include <QTimer>
+#include <QMutex>
+#include <TWebApplication>
+#include "tsendmailmailer.h"
+#include "tsystemglobal.h"
+
+static QMutex sendMutex;
+
+/*!
+  \class TSendmailMailer
+  \brief The TSendmailMailer class provides a simple functionality to send
+  emails by sendmail command.
+*/
+
+TSendmailMailer::TSendmailMailer(const QString &command, QObject *parent)
+    : QObject(parent), sendmailCmd(command), mailMessage()
+{ }
+
+
+TSendmailMailer::~TSendmailMailer()
+{
+    T_TRACEFUNC("");
+    if (!mailMessage.isEmpty()) {
+        tSystemWarn("Mail not sent. Deleted it.");
+    }
+}
+
+
+bool TSendmailMailer::send(const TMailMessage &message)
+{
+    T_TRACEFUNC("");
+
+    mailMessage = message;
+    bool res = send();
+    mailMessage.clear();
+    return res;
+}
+
+
+void TSendmailMailer::sendLater(const TMailMessage &message)
+{
+    T_TRACEFUNC("");
+
+    mailMessage = message;
+    QTimer::singleShot(0, this, SLOT(sendAndDeleteLater()));
+}
+
+
+void TSendmailMailer::sendAndDeleteLater()
+{
+    T_TRACEFUNC("");
+
+    send();
+    mailMessage.clear();
+    deleteLater();
+}
+
+
+bool TSendmailMailer::send()
+{
+    QMutexLocker locker(&sendMutex); // Global lock for load reduction of mail server
+    if (sendmailCmd.isEmpty()) {
+        return false;
+    }
+
+    QStringList args;
+    QByteArray rawmail = mailMessage.toByteArray();
+    QList<QByteArray> recipients = mailMessage.recipients();
+
+    for (QListIterator<QByteArray> it(recipients); it.hasNext(); ) {
+        const QByteArray recipt = it.next();
+        args.clear();
+        args << recipt;
+
+        QProcess sendmail;
+        sendmail.start(sendmailCmd, args);
+        if (!sendmail.waitForStarted(5000)) {
+            tSystemError("Sendmail error. CMD: %s", qPrintable(sendmailCmd));
+            return false;
+        }
+
+        sendmail.write(rawmail);
+        sendmail.write("\n.\n");
+        sendmail.waitForFinished();
+        tSystemDebug("Mail sent. Recipients: %s", recipt.data());
+    }
+
+    return true;
+}
---
+++ src/tsendmailmailer.h
@@ -0,0 +1,35 @@
+#ifndef TSENDMAILMAILER_H
+#define TSENDMAILMAILER_H
+
+#include <QObject>
+#include <QStringList>
+#include <QByteArray>
+#include <QProcess>
+#include <TMailMessage>
+
+
+class T_CORE_EXPORT TSendmailMailer : public QObject
+{
+    Q_OBJECT
+public:
+    TSendmailMailer(const QString &command, QObject *parent = 0);
+    ~TSendmailMailer();
+
+    QString key() const { return "sendmail"; }
+    bool send(const TMailMessage &message);
+    void sendLater(const TMailMessage &message);
+
+protected slots:
+    void sendAndDeleteLater();
+
+protected:
+    bool send();
+
+private:
+    QString sendmailCmd;
+    TMailMessage mailMessage;
+
+    Q_DISABLE_COPY(TSendmailMailer)
+};
+
+#endif // TSENDMAILMAILER_H
