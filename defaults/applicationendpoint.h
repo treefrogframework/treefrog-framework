72ab00268ae9c8f1ae74dbeb747a4f3698f0a4a3
AOYAMA Kazuharu <aoyama@ideeinc.co.jp>
2015-05-01 09:14:21 +0900

    delete 'override' keyword.

--- defaults/applicationendpoint.h
+++ defaults/applicationendpoint.h
@@ -12,14 +12,14 @@ public:
     virtual ~ApplicationEndpoint() { }
 
 protected:
-    virtual bool onOpen(const TSession &httpSession) override;
-    virtual void onClose(int closeCode) override;
-    virtual void onTextReceived(const QString &text) override;
-    virtual void onBinaryReceived(const QByteArray &binary) override;
-    virtual void onPing(const QByteArray &data) override;
-    virtual void onPong(const QByteArray &data) override;
-    virtual int keepAliveInterval() const override;
-    virtual bool transactionEnabled() const override;
+    virtual bool onOpen(const TSession &httpSession);
+    virtual void onClose(int closeCode);
+    virtual void onTextReceived(const QString &text);
+    virtual void onBinaryReceived(const QByteArray &binary);
+    virtual void onPing(const QByteArray &data);
+    virtual void onPong(const QByteArray &data);
+    virtual int keepAliveInterval() const;
+    virtual bool transactionEnabled() const;
 };
 
 #endif // APPLICATIONENDPOINT_H
