5a09b2d70b5d680f101bdad1d46616e6a249052d
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2016-03-05 18:34:52 +0900

    update bat

--- build.bat
+++ build.bat
@@ -4,9 +4,9 @@
 ::
 :: Edit this line to run the batch file for Qt environment.
 ::
-::call "C:\Qt\Qt5.4.2-MinGW\5.4\mingw491_32\bin\qtenv2.bat"
+::call "C:\Qt\Qt5.4.1-MinGW\5.4\mingw491_32\bin\qtenv2.bat"
 ::call "C:\Qt\Qt5.5.1-MinGW\5.5\mingw492_32\bin\qtenv2.bat"
-::call "C:\Qt\Qt5.4.0-msvc2013_64\5.4\msvc2013_64_opengl\bin\qtenv2.bat"
+::call "C:\Qt\Qt5.4.1\5.4\msvc2013_64_opengl\bin\qtenv2.bat"
 call "C:\Qt\Qt5.5.1\5.5\msvc2013_64\bin\qtenv2.bat"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
