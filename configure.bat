0278793647a9b99f874cbfe7606d70115b1ef78c
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2019-06-06 14:25:33 +0900

    update configure.bat

--- configure.bat
+++ configure.bat
@@ -142,9 +142,9 @@ mklink /d lz4 lz4-%LZ4_VERSION% >nul 2>&1
 if not "%MSCOMPILER%" == "" (
   for /F %%i in ('qtpaths.exe --install-prefix') do echo %%i | find "msvc2015" >NUL
   if not ERRORLEVEL 1 (
-    devenv lz4\visual\VS2015\lz4.sln /project liblz4 /rebuild "Release|x64"
+    devenv lz4\visual\VS2015\lz4.sln /project liblz4 /rebuild "Release|%VCVARSOPT%"
   ) else (
-    devenv lz4\visual\VS2017\lz4.sln /project liblz4 /rebuild "Release|x64"
+    devenv lz4\visual\VS2017\lz4.sln /project liblz4 /rebuild "Release|%VCVARSOPT%"
   )
 ) else (
   cd lz4\lib
