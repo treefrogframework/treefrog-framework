883493c1d8ae1f93849b46ce297c5cb00f69fc24
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2019-06-24 22:15:34 +0900

    typo

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ rem call "C:\Qt\5.12.3\msvc2015_64\bin\qtenv2.bat"
 rem call "C:\Qt\5.12.3\mingw73_64\bin\qtenv2.bat"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-remcall "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 cd /D %~dp0
