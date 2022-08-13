34f482a64191907ad07be919bba468a6deb37ff0
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2022-08-13 10:12:49 +0900

    updated version strings.

--- src/tglobal.h
+++ src/tglobal.h
@@ -1,7 +1,7 @@
 #pragma once
 constexpr auto TF_VERSION_STR = "2.4.0";
 constexpr auto TF_VERSION_NUMBER = 0x020400;
-constexpr auto TF_SRC_REVISION = 2542;
+constexpr auto TF_SRC_REVISION = 2549;
 
 #include <QMetaType>
 #include <QIODevice>
