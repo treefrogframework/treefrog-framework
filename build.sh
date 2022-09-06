487b432fa60e798bad47b2493f8f2e8b87a5de01
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2022-09-06 20:29:54 +0900

    fix script.

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 # Build script for CodeQL on Ubuntu
 
 sudo apt-get update -qq
-sudo apt-get install -y --no-install-recommends qmake6 qt6-base-dev qt6-base-dev-tools qt6-tools-dev-tools qt6-declarative-dev libqt6sql6-mysql libqt6sql6-psql libqt6sql6-odbc libqt6sql6-sqlite libqt6core6 libqt6qml6 libqt6xml6 libpq5 libodbc1 libmongoc-dev libbson-dev gcc g++ clang make cmake
-
-sudo rm -f /usr/bin/qmake
-sudo ln -sf /usr/bin/qmake6 /usr/bin/qmake
+sudo apt-get install -y --no-install-recommends qtbase5-dev qt5-default qt5-qmake qttools5-dev-tools qtdeclarative5-dev qtdeclarative5-dev-tools libqt5sql5 libqt5sql5-sqlite libsqlite3-dev libmongoc-dev libbson-dev gcc g++ clang make cmake
 
 ./configure --spec=linux-clang
 make -j10 -C src && sudo make -C src install && make -j10 -C tools && sudo make -C tools install
