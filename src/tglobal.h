233ae6ae84166826b74ba6fb358c1bde626b9724
aoyama <a.kazuharu@gmail.com>
2023-01-17 23:38:37 +0900

    updated version strings.

--- src/tglobal.h
+++ src/tglobal.h
@@ -1,7 +1,7 @@
 #pragma once
-constexpr auto TF_VERSION_STR = "2.7.0";
-constexpr auto TF_VERSION_NUMBER = 0x020700;
-constexpr auto TF_SRC_REVISION = 2752;
+constexpr auto TF_VERSION_STR = "2.6.1";
+constexpr auto TF_VERSION_NUMBER = 0x020601;
+constexpr auto TF_SRC_REVISION = 2756;
 
 #include <QMetaType>
 #include <QIODevice>
