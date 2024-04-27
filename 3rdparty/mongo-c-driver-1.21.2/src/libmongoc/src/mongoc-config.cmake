14edf8c650db4fe11ae261283bf01d89ad8a031b
aoyama <a.kazuharu@gmail.com>
2024-04-27 12:32:34 +0900

    delete mongo-c-driver-1.21.2

--- 3rdparty/mongo-c-driver-1.21.2/src/libmongoc/src/mongoc-config.cmake
+++
@@ -1,3 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(bson-1.0 @MONGOC_MAJOR_VERSION@.@MONGOC_MINOR_VERSION@.@MONGOC_MICRO_VERSION@)
-include("${CMAKE_CURRENT_LIST_DIR}/mongoc-targets.cmake")
