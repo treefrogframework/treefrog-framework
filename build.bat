8c120c33dc28eb5233441d0c203003974780dee0
aoyama <a.kazuharu@gmail.com>
2024-11-24 10:08:19 +0900

    update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+@echo off
 @setlocal
 
 rem
@@ -7,14 +7,13 @@ rem
 
 set BASEDIR=%~dp0
 
-call "C:\Qt\6.8.0\msvc2022_64\bin\qtenv2.bat"
+call "C:\Qt\6.7.3\msvc2022_64\bin\qtenv2.bat"
 rem call "C:\Qt\6.5.3\msvc2019_64\bin\qtenv2.bat"
-rem call "C:\Qt\5.13.0\msvc2017\bin\qtenv2.bat"
 
 set ARCH=amd64
 set VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 set VCVARSBAT=""
-set VSVER=2022 2019 2017
+set VSVER=2019
 set PATH=C:\Qt\Tools\CMake_64\bin;%PATH%
 
 if exist %VSWHERE% (
@@ -34,7 +33,7 @@ if exist %VCVARSBAT% (
   echo %VCVARSBAT% %ARCH%
   call %VCVARSBAT% %ARCH%
 ) else (
-  echo Error!  Compiler not found.
+  echo Error! Visual Studio not found.
   pause
   exit
 )
